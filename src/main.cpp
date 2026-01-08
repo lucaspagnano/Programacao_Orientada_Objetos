9d8526d1eb64e394bf58fade357c0d6375392357
lucaspagnano <lucas.pagnano@gmail.com>
2026-01-08 15:57:12 +0000

    Formatacao simples

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ void menu() {
     std::cout << "\n" << std::string(85, '-') << "\n";
     std::cout << std::left << std::setw(cmdWidth) << "COMANDO" << " | " << "DESCRICAO" << "\n";
     std::cout << std::string(85, '-') << "\n";
-    std::cout << "** SISTEMA E SIMULACAO **\n";
     std::cout << std::setw(cmdWidth) << "jardim <l> <c>" << " | " << "Cria o jardim com 'l' linhas e 'c' colunas (1o comando).\n";
     std::cout << std::setw(cmdWidth) << "avanca [n]" << " | " << "Avanca 'n' instantes na simulacao (default: 1).\n";
     std::cout << std::setw(cmdWidth) << "executa <nome>" << " | " << "Executa lista de comandos de um ficheiro de texto.\n";
@@ -19,20 +18,14 @@ void menu() {
     std::cout << std::setw(cmdWidth) << "recupera <nome>" << " | " << "Recupera um estado do jardim guardado anteriormente.\n";
     std::cout << std::setw(cmdWidth) << "apaga <nome>" << " | " << "Apaga uma gravacao da memoria.\n";
     std::cout << std::setw(cmdWidth) << "fim" << " | " << "Termina a simulacao e liberta recursos.\n";
-    std::cout << "\n";
-    std::cout << "** MOVIMENTACAO **\n";
     std::cout << std::setw(cmdWidth) << "entra <l><c>" << " | " << "Jardineiro entra no jardim na posicao (linha, coluna).\n";
     std::cout << std::setw(cmdWidth) << "sai" << " | " << "Jardineiro sai do jardim.\n";
     std::cout << std::setw(cmdWidth) << "e / d / c / b" << " | " << "Move jardineiro (Esq, Dir, Cima, Baixo). Max 10/turno.\n";
-    std::cout << "\n";
-    std::cout << "** INFORMACOES **\n";
     std::cout << std::setw(cmdWidth) << "lplantas" << " | " << "Lista todas as plantas vivas e seus atributos.\n";
     std::cout << std::setw(cmdWidth) << "lplanta <l><c>" << " | " << "Mostra detalhes da planta numa posicao especifica.\n";
     std::cout << std::setw(cmdWidth) << "larea" << " | " << "Lista conteudo/propriedades de posicoes nao vazias.\n";
     std::cout << std::setw(cmdWidth) << "lsolo <l><c> [n]" << " | " << "Detalhes do solo (opcional: quadrado de raio n).\n";
     std::cout << std::setw(cmdWidth) << "lferr" << " | " << "Lista as ferramentas na posse do jardineiro.\n";
-    std::cout << "\n";
-    std::cout << "** ACOES **\n";
     std::cout << std::setw(cmdWidth) << "colhe" << " | " << "Colhe a planta na posicao atual (Max 5 por turno).\n";
     std::cout << std::setw(cmdWidth) << "planta <l><c> <t>" << " | " << "Planta especie (c,r,e,x) na posicao (Max 2 por turno).\n";
     std::cout << std::setw(cmdWidth) << "compra <t>" << " | " << "Compra uma ferramenta do tipo g, a, t ou z.\n";
